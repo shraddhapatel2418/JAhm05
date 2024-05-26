fd3e860f54bd160baf96454dcd6e59e7078a347d
Patel Shraddha <169138973+shraddhapatel2418@users.noreply.github.com>
2024-05-26 15:34:00 +0530

    Add files via upload

---
+++ geometry/src/geometry/Triangle.java
@@ -0,0 +1,22 @@
+package geometry;
+
+public class Triangle {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public double getArea() {
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+}
