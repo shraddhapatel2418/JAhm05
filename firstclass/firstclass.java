e72c0bf303d8f37ebd721bd787577aa821fb6522
Patel Shraddha <169138973+shraddhapatel2418@users.noreply.github.com>
2024-05-26 15:40:43 +0530

    Add files via upload

---
+++ firstclass/firstclass.java
@@ -0,0 +1,20 @@
+package firstclass;
+
+public class firstclass {
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int result;
+		
+		result=a+b;
+		System.out.println(a +"+"+ b+" ==> "+result);
+		
+		result=b-a;
+		System.out.println(b +"-"+ a+" ==> "+result);
+		
+		result=a*b;
+		System.out.println(a +"*"+ b+" ==> "+result);
+		
+		result=b/a;
+		System.out.println(a +"/"+ b+" ==> "+result);
+	}
+}
